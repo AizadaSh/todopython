e475d7d0d56a013b508f3192148a7f695cc6f4da
AizadaSh <shabieva.ai@gmail.com>
2021-01-23 23:00:11 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -41,4 +41,10 @@ def mark_todo(request, id):
     return redirect(test)
 
 
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
  
